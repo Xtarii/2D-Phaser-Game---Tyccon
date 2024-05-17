94a625ed269d07ea02a339dee100ee0b5378090d
Xtarii <carlalvinelias@gmail.com>
2024-05-17 14:42:33 +0200

    Game DEBUG: TEST ONLY

--- ts.src/server/routes/home.ts
+++ ts.src/server/routes/home.ts
@@ -22,6 +22,10 @@ route.get("/", (req, res) => res.render("index", {title: "Home"})) // Main
 
 
 
+// TEST
+route.get("/game", (req, res) => res.render("game", {title: "Game"}))
+
+
 
 // route.get("/license", (req, res) => res.render("license", { title: "License" }))
 
