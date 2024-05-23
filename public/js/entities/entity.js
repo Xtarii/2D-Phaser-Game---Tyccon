c2803b069ded8a8feab328fb3820c33602d11c2c
Xtarii <carlalvinelias@gmail.com>
2024-05-23 10:36:19 +0200

    player speed

--- public/js/entities/entity.js
+++ public/js/entities/entity.js
@@ -6,10 +6,10 @@ export class Entity extends Physics.Arcade.Sprite {
     /**
      * Entity Speed
      */
-    speed = 200
-    /** 
-     * Entity damp speed 
-     * 
+    speed = 75
+    /**
+     * Entity damp speed
+     *
      * Damping Entity speed, to deley abropt stop
     */
     dampSpeed = 0.9
