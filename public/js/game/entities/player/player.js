44bf08758be4159ecd086c623a0d176d2e74d8bc
Xtarii <carlalvinelias@gmail.com>
2024-05-28 12:29:48 +0200

    Fixed? Server Error

--- public/js/game/entities/player/player.js
+++ public/js/game/entities/player/player.js
@@ -57,6 +57,18 @@ export class Player extends Entity {
 
 
         this.keys = this.scene.input.keyboard.addKeys("W,S,A,D")
+
+
+
+
+        // Connection to Server
+        Game.server.socket.on("connect", () => {
+            // Local Client Data
+            Game.server.socket.emit("prespawn clientData", {
+                x: MainScene.player.x,
+                y: MainScene.player.y
+            })
+        })
     }
 
 
--- public/js/game/networking/server.js
+++ public/js/game/networking/server.js
@@ -33,52 +33,45 @@ export class Server {
         else this.socket = Socket.io(host) // Connects to Socket Server
 
 
-        // Local Client Connection Handler
-        this.socket.on("connect", () => {
-            // Local Client Data
-            this.socket.emit("prespawn clientData", {
-                x: MainScene.player.x | 0,
-                y: MainScene.player.y | 0
-            })
 
-            // Spawns Clients
-            this.socket.on("spawn clients", (data) =>  {
-                for (let i in data) {
-                    if (data[i].id !== this.socket.id){
-                        // Adds Client to scene
-                        const obj = game.scene.getScene("main").add.image(
-                            data[i].position.x, data[i].position.y, "player")
+        // Spawns Clients
+        this.socket.on("spawn clients", (data) =>  {
+            for (let i in data) {
+                if (data[i].id !== this.socket.id){
+                    // Adds Client to scene
+                    const obj = game.scene.getScene("main").add.image(
+                        data[i].position.x, data[i].position.y, "player")
 
-                        this.clients[i] = {
-                            body: obj
-                        }
+                    this.clients[i] = {
+                        body: obj
                     }
                 }
-            })
-            this.socket.on("spawn client", (data) => {
-                // Adds Client to scene
-                const obj = game.scene.getScene("main").add.image(
-                    data.position.x, data.position.y, "player")
+            }
+        })
+        // Spawn New Client
+        this.socket.on("spawn client", (data) => {
+            // Adds Client to scene
+            const obj = game.scene.getScene("main").add.image(
+                data.position.x, data.position.y, "player")
 
-                this.clients[data.id] = {
-                    body: obj
-                }
-            })
-
-
-            // Client Update
-            this.socket.on("update client", (data) => {
-                // Updates client
-                this.clients[data.id].body.x = data.position.x
-                this.clients[data.id].body.y = data.position.y
-            })
+            this.clients[data.id] = {
+                body: obj
+            }
+        })
 
 
-            // Client Despawn
-            this.socket.on("despawn client", (id) => {
-                this.clients[id].body.destroy(true)
-                delete this.clients[id]
-            })
+        // Client Update
+        this.socket.on("update client", (data) => {
+            // Updates client
+            this.clients[data.id].body.x = data.position.x
+            this.clients[data.id].body.y = data.position.y
+        })
+
+
+        // Client Despawn
+        this.socket.on("despawn client", (id) => {
+            this.clients[id].body.destroy(true)
+            delete this.clients[id]
         })
     }
 }
