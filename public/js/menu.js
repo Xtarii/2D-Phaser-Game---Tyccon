41d238baee5f986545c01843737667fd93791d19
Xtarii <carlalvinelias@gmail.com>
2024-05-29 20:21:34 +0200

    Player Skin and Name Selection

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,4 +1,13 @@
 document.getElementById("connect").addEventListener("click", () => {
+    // Player Info
+    let name = document.getElementById("name").value
+    let spriteID = document.getElementById("spriteID").value
+
+    localStorage.setItem("playerName", name === "" ? ("Dummy" + Math.random() * (1000 - 10) + 10) : name)
+    localStorage.setItem("spriteID", spriteID === "" ? "player" : spriteID)
+
+
+    // Game Start
     localStorage.setItem("host", document.getElementById("server").value)
     location.href = "/game"
 })
