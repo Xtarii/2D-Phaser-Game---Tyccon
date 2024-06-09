fc1b83abf7871a52c8a18246cfdc3794d4e128ca
Xtarii <carlalvinelias@gmail.com>
2024-06-09 22:35:42 +0200

    UI Base Class

---
+++ public/js/ui/gui.js
@@ -0,0 +1,12 @@
+const { GameObjects } = require("phaser")
+
+
+
+/**
+ * UI Base Class
+ *
+ * All UI Components should Extend Upon this class
+ * unless, if I ( Alvin ), decide to do my other UI
+ * Component Builder Class
+ */
+export default class UI extends GameObjects.GameObject {}
