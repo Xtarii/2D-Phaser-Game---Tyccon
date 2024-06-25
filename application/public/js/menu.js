ab485bfa49a8f246b14a2ea944c6d8d869713707
Xtarii <carlalvinelias@gmail.com>
2024-06-25 21:43:58 +0200

    Replaced Player Local Storage

--- application/public/js/menu.js
+++ application/public/js/menu.js
@@ -1,41 +1,22 @@
-import { readPlayerInfo } from "./utils/data/playerDataHandler.js"
+const { PlayerData } =  require("@obesity/utils")
 
 
-
-/**
- * Player Character Information
- */
-const playerInfo = {
-    /**
-     * Player Name
-     */
-    name: ("Dummy" + Math.ceil(Math.random() * (1000 - 1 ) + 1)),
-    /**
-     * Player Sprite ID
-     */
-    spriteID: "player",
-}
 
 // Reads Player Info
-playerInfo.name = readPlayerInfo("name") || playerInfo.name
-playerInfo.spriteID = readPlayerInfo("spriteID") || playerInfo.spriteID
-
+const playerData = PlayerData.readPlayerData()
 
 // Applies Data to Front-end Inputs
-document.getElementById("name").value = playerInfo.name
-document.getElementById("spriteID").value = playerInfo.spriteID
+document.getElementById("name").value = playerData.name
+document.getElementById("spriteID").value = playerData.spriteID
 
 
 // Connection Event
 document.getElementById("connect").addEventListener("click", () => {
     // Player Info
-    let name = document.getElementById("name").value
-    let spriteID = document.getElementById("spriteID").value
+    const name = document.getElementById("name").value
+    const spriteID = document.getElementById("spriteID").value
 
-    playerInfo.name = name === "" ? ("Dummy" + Math.ceil(Math.random() * (1000 - 1 ) + 1)) : name
-    playerInfo.spriteID = spriteID === "" ? "player" : spriteID
-
-    localStorage.setItem("player info", JSON.stringify(playerInfo)) // Saves Player Info
+    PlayerData.storePlayerData({ name, spriteID }) // Stores Player Data
 
 
     // Game Start
