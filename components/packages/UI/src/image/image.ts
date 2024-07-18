c8e7fda3d277e828c7ecaf156bd0a9e6dab4c3d0
Xtarii <carlalvinelias@gmail.com>
2024-07-18 17:23:19 +0200

    Added Image UI

---
+++ components/packages/UI/src/image/image.ts
@@ -0,0 +1,32 @@
+import { Scene } from "phaser"
+import { PlacementType, TINT, UISprite } from ".."
+
+
+
+/**
+ * UI Image Object
+ *
+ * An UI Image will not contain any extra
+ * functions, it will just be an image on
+ * the screen that is part of the
+ * UI component Tree.
+ */
+export class Image extends UISprite {
+    /**
+     * Creates UI Image Instance
+     *
+     * @param args UI Args
+     */
+    constructor(...args: [Scene, number, number, string, (number | string)?, PlacementType?]) {
+        super(args[0], args[1], args[2], args[3], args[4], args[5]) // Creates UI Sprite
+    }
+
+
+
+    /**
+     * Sets Image Tint
+     *
+     * @param tint TINT
+     */
+    setTint = (tint: TINT | number) => { this.sprite.setTint(tint) }
+}
