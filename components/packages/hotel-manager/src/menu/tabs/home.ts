7d4672b45854f63686cb5d4009986368a07847cc
Xtarii <carlalvinelias@gmail.com>
2024-08-01 23:06:06 +0200

    Hotel Manager Tabs

--- components/packages/hotel-manager/src/menu/tabs/home.ts
+++ components/packages/hotel-manager/src/menu/tabs/home.ts
@@ -4,8 +4,10 @@ import { Tab } from "../tab"
 
 export default class Home extends Tab.TabObject {
     open(): void {
+        // Display Hotel Information
     }
 
     close(): void {
+        // Remove UI
     }
 }
