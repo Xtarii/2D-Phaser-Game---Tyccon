260e0956f990f3183542fcd0b08717b1dd085774
Xtarii <carlalvinelias@gmail.com>
2024-08-10 20:09:59 +0200

    Cleanup

--- components/packages/room-manager/src/room.ts
+++ components/packages/room-manager/src/room.ts
@@ -1,5 +1,6 @@
-import { gridPosition } from "obesity-utils"
+import { storeRoomData } from "obesity-utils"
 import { GameObjects, Scene } from "phaser"
+import { Room as room } from "obesity-utils"
 
 
 
@@ -12,32 +13,20 @@ import { GameObjects, Scene } from "phaser"
  */
 export class Room extends GameObjects.Sprite {
     /**
-     * Rooms Door Position
+     * Room Data
      *
-     * The Position of the Rooms
-     * Door on the grid.
+     * JSON Data that holds
+     * the rooms
+     * - level
+     * - cost
+     * - door position
+     * and more.
      */
-    public readonly grid: gridPosition
-    /**
-     * Room Level
-     *
-     * The Level of the room.
-     * This decides what room
-     * to load.
-     */
-    private _level: number
-    /**
-     * Room Type
-     *
-     * The Type of the room.
-     * This decides what type
-     * of room to load.
-     */
-    public readonly roomType: string
+    public readonly roomData: room
 
 
 
-    constructor(scene: Scene, position: gridPosition, level: number, type: string, name: string) {
+    constructor(scene: Scene, id: string, data: room) {
         /// Uses Grid Position as Door Position
         ///
         /// To Convert to a position vector ( x, y )
@@ -47,15 +36,16 @@ export class Room extends GameObjects.Sprite {
         ///
         /// Result: (axis * SpriteSize) - (SpriteSize / 2)
         /// That is the center of the grid.
-        super(scene, position.x * 64 - 32, position.y * 64 - 32, "interact key")
+        super(scene, data.door.x * 64 - 32, data.door.y * 64 - 32, "interact key")
         scene.add.existing(this)
         this.setDepth(-5) // Hides Door from Map
 
         // Room Data
-        this.grid = position
-        this._level = level
-        this.roomType = type
-        this.name = name
+        this.roomData = data
+        this.name = id
+
+        if(!this.roomData.level) this.roomData.level = 1
+        if(!this.roomData.status) this.roomData.status = "built"
 
         this.saveData() // Saves Room Data
     }
@@ -70,7 +60,7 @@ export class Room extends GameObjects.Sprite {
      * @param level Level
      */
     public upgrade(level: number) {
-        this._level = level
+        this.roomData.level = level
         this.saveData() // Saves Room Data
     }
 
@@ -81,6 +71,8 @@ export class Room extends GameObjects.Sprite {
      */
     protected saveData() {
         console.log("Saving Room...")
+
+        storeRoomData(1, {id: this.name, room: this.roomData }) // Stores Data
     }
 
 
@@ -94,5 +86,5 @@ export class Room extends GameObjects.Sprite {
      * This decides what room
      * to load.
      */
-    get level(): number { return this._level }
+    get level(): number | undefined { return this.roomData.level }
 }
