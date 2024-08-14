4d716efb42c6d88e03c9650e2a2c5ba4318df7cf
Xtarii <carlalvinelias@gmail.com>
2024-08-14 07:28:08 +0200

    Extends Scene

--- components/packages/world/src/index.ts
+++ components/packages/world/src/index.ts
@@ -1,1 +1,15 @@
-export class World {}
+import { Scene } from "phaser"
+
+
+
+/**
+ * ### World Scene Manager
+ *
+ * Controls Scene loading and
+ * the management of tilemaps,
+ * tilesets and the loading and
+ * unloading of instances in
+ * the World ( Scene ).
+ */
+export class World extends Scene {
+}
