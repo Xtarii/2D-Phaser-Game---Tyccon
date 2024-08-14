99edd4f72e8855f1e39eded126ecddf6c84729a1
Xtarii <carlalvinelias@gmail.com>
2024-08-14 07:19:24 +0200

    Created a World Manager Component

--- components/src/index.ts
+++ components/src/index.ts
@@ -5,3 +5,6 @@ export * from "@obesity-components/entity"
 // Hotel Manager Components
 export * from "@obesity-components/hotel-manager"
 export * from "@obesity-components/room-manager"
+
+// Scene Manager Components
+export * from "@obesity-components/world"
