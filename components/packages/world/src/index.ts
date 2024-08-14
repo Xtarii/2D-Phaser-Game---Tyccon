79e63c333555e79e24dc61c6efc284cd9b813bef
Xtarii <carlalvinelias@gmail.com>
2024-08-14 10:37:40 +0200

    New Scene System

--- components/packages/world/src/index.ts
+++ components/packages/world/src/index.ts
@@ -1,4 +1,7 @@
-import { Scene } from "phaser"
+import { GameObjects, Scene } from "phaser"
+import { SceneObject } from "./scene/scene"
+import { WorldManager } from "./world/world"
+import { scenes } from "./scene/sceneManager"
 
 
 
@@ -12,4 +15,59 @@ import { Scene } from "phaser"
  * the World ( Scene ).
  */
 export class World extends Scene {
+    /**
+     * Loads Scene Object
+     *
+     * **Recommended** to create a
+     * scene and load it using
+     * ```ts
+     * loadScene("key")
+     *
+     * // or
+     * loadScene(scenes.get("key"))
+     *
+     *
+     * // NOT
+     * loadScene(new SceneObject(...params))
+     * ```
+     *
+     * @param scene Scene Object
+     */
+    loadScene(scene: SceneObject) : void
+    /**
+     * Loads Scene from Scene List
+     *
+     * @param key Scene Key
+     */
+    loadScene(key: string) : void
+
+
+    loadScene(scene: SceneObject | string) {
+        if(scene instanceof SceneObject) {
+            WorldManager.loadSceneObject(this, scene)
+        }else {
+            WorldManager.loadSceneObject(this, scenes.get(scene))
+        }
+    }
+
+
+
+    /**
+     * Add Collidable Object to the Scene
+     *
+     * Only add the object if it should
+     * collide with the world and scene.
+     *
+     * @param object Object
+     */
+    addCollidableObject = (object: GameObjects.GameObject) => { WorldManager.addCollidable(object) }
 }
+
+
+
+
+
+// Library Export
+export * from "./scene/scene"
+export * from "./scene/sceneManager"
+export * from "./world/world"
