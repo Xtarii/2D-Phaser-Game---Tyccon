9fa2bc752a594a6f9a882c7ef1ff0942c66d439e
Xtarii <carlalvinelias@gmail.com>
2024-08-30 11:33:18 +0200

    Network Object Level

--- application/public/js/game/networking/networkObjects/networkPlayer.js
+++ application/public/js/game/networking/networkObjects/networkPlayer.js
@@ -1,3 +1,4 @@
+const { Runtime } = require("obesity-utils")
 const { Entity } = require("obesity-components")
 
 
@@ -8,5 +9,15 @@ const { Entity } = require("obesity-components")
 export default class NetworkPlayer extends Entity {
     constructor(scene, x, y, l, spriteID, name, depth){
         super(scene, x, y, l, spriteID, null, name, depth) // Parent ( Entity ) Setup
+    }
+
+
+
+    update() {
+        super.update()
+
+        // Network Render Level Check
+        if(this.level !== Runtime.Player.getLocation().l) this.setVisible(false)
+        else this.setVisible(true)
     }
 }
