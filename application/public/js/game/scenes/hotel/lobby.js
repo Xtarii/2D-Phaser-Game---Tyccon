b12fb2d6b448a3a9059d6830e4d4bc67b7b29a5a
Xtarii <carlalvinelias@gmail.com>
2024-09-02 13:28:26 +0200

    Hotel Lobby 1

---
+++ application/public/js/game/scenes/hotel/lobby.js
@@ -0,0 +1,35 @@
+import { Game } from "../../game.js"
+import MainScene from "../../world/scenes/mainScene.js"
+
+const { SceneObject } = require("obesity-components")
+
+
+
+export default class Lobby extends SceneObject {
+    constructor(map) {
+        super(map, {tileName: "Hotel tiles", key: "hotel tileset"})
+    }
+
+    onCreate() {
+        this.addLayer({name: "ground"}, "Hotel tiles")
+        this.addLayer({name: "wall", collision: true}, "Hotel tiles")
+        this.addLayer({name: "wall_over", collision: true}, "Hotel tiles")
+    }
+
+    onLoad(s) {
+        /** @type {World} */
+        const scene = s
+
+        // Gets Room Data
+        if(Game.server.room) Game.server.room.send("get level data", 1)
+        else scene.setupDoors(1)
+
+
+        // Teleport player
+        const x = 64 * 8.5 - 32, y = 64 * 14 - 32
+        if(MainScene.player) MainScene.player.setPosition(
+            x + Math.random() * ((64 * 2) - -(64 * 2)) + -(64 * 2),
+            y + Math.random() * ((64 * 2) - -(64 * 2)) + -(64 * 2)
+        )
+    }
+}
