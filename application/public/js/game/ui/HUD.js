319ef154bf8a667c67e407b1948175707842108c
Xtarii <carlalvinelias@gmail.com>
2024-09-04 13:34:10 +0200

    Player money Position

--- application/public/js/game/ui/HUD.js
+++ application/public/js/game/ui/HUD.js
@@ -13,7 +13,7 @@ export default class HUD {
      * @param {Phaser.Scene} scene Game Scene
      */
     constructor(scene) {
-        this.money = new Text(scene, 450, 10, (Runtime.Player.getMoney() + " B"))
-        this.money.x = 450 - this.money.displayWidth
+        this.money = new Text(scene, scene.cameras.main.displayWidth, 10, (Runtime.Player.getMoney() + " B"))
+        this.money.x = scene.cameras.main.displayWidth - this.money.displayWidth
     }
 }
